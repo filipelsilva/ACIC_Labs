038dadce9ac741e2f63987c9c9ba7334d27a05a5
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-12-01 18:11:12 +0000

    Apparently the professors want calibration, huh

--- Lab2/master/master.ino
+++ Lab2/master/master.ino
@@ -4,16 +4,54 @@
 #define LIGHTSENSORPIN A1
 #define POTENTIOMETERPIN A3
 
+int temperatureSensorMin = 1023;
+int temperatureSensorMax = 0;
+int angleSensorMin = 1023;
+int angleSensorMax = 0;
+int luminositySensorMin = 1023;
+int luminositySensorMax = 0;
+
+void calibrateSensors() {
+  while (millis() < 10000) {
+    int temperatureRead = analogRead(TEMPERATURESENSORPIN);
+    int angleRead = analogRead(POTENTIOMETERPIN);
+    int luminosityRead = analogRead(LIGHTSENSORPIN);
+
+    if (temperatureRead < temperatureSensorMin) {
+      temperatureSensorMin = temperatureRead;
+    }
+    if (temperatureRead > temperatureSensorMax) {
+      temperatureSensorMax = temperatureRead;
+    }
+    if (angleRead < angleSensorMin) {
+      angleSensorMin = angleRead;
+    }
+    if (angleRead > angleSensorMax) {
+      angleSensorMax = angleRead;
+    }
+    if (luminosityRead < luminositySensorMin) {
+      luminositySensorMin = luminosityRead;
+    }
+    if (luminosityRead > luminositySensorMax) {
+      luminositySensorMax = luminosityRead;
+    }
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  calibrateSensors();
 }
 
 void loop() {
   // Read the values from the sensors
-  int temperatureRead = analogRead(TEMPERATURESENSORPIN);
-  int angleRead = analogRead(POTENTIOMETERPIN);
-  int luminosityRead = analogRead(LIGHTSENSORPIN);
+  // int temperatureRead = analogRead(TEMPERATURESENSORPIN);
+  // int angleRead = analogRead(POTENTIOMETERPIN);
+  // int luminosityRead = analogRead(LIGHTSENSORPIN);
+  int temperatureRead = map(analogRead(TEMPERATURESENSORPIN), temperatureSensorMin, temperatureSensorMax, 0, 1023);
+  int angleRead = map(analogRead(POTENTIOMETERPIN), angleSensorMin, angleSensorMax, 0, 1023);
+  int luminosityRead = map(analogRead(LIGHTSENSORPIN), luminositySensorMin, luminositySensorMax, 0, 1023);
 
   // Send them over the wire
   Wire.beginTransmission(8);
